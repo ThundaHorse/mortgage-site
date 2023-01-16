7d21b7e21e92ea8a5f1e3fc5b074607cf35b90cd
Abraham Kim <akim@insurancecredirect.com>
2023-01-16 15:22:27 -0500

    title, scroll into view, no hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
@@ -19,9 +19,12 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView({ behavior: 'smooth' });
+  },
 });
 
 export default router;
